d0acc1a4ada31380cb9e72018ee5d85954bf9029
glgraca <30938064+glgraca@users.noreply.github.com>
2017-08-11 20:28:06 -0300

    Update piframebot.pl

--- piframebot.pl
+++ piframebot.pl
@@ -24,7 +24,7 @@ my $api=WWW::Telegram::BotAPI->new (
     token => $token
 );
 
-#Verify is user has permission to use this bot
+#Verify that user has permission to use this bot
 sub verify_user {
   my $u=shift;
   my $id=$u->{message}{from}{id};
@@ -66,30 +66,31 @@ while (1) {
     if(!verify_user($u)) {
       #If you are not allowed, you get your id back, so you may ask for permission
       $api->sendMessage({chat_id=>$u->{message}{from}{id}, text=>'Hello, '.$u->{message}{from}{id}});
-    }
-    if(my $photos = $u->{message}{photo}) {
-      my $photo=$photos->[0];
-      for my $p (@$photos) {
-        $photo=$p if $p->{file_size}>$photo->{file_size};
+    } else {
+      if(my $photos = $u->{message}{photo}) {
+        my $photo=$photos->[0];
+        for my $p (@$photos) {
+          $photo=$p if $p->{file_size}>$photo->{file_size};
+        }
+        eval {
+          my $file_desc=$api->getFile({file_id=>$photo->{file_id}});
+          my $file_path=$file_desc->{result}->{file_path};
+          `curl -s -k -o ${path}/${update_id}.jpg https://api.telegram.org/file/bot$token/$file_path`;
+          $res="Image was saved as ${path}/${update_id}.jpg";
+        };
+        $res="Error: $@" if $@;
       }
-      eval {
-        my $file_desc=$api->getFile({file_id=>$photo->{file_id}});
-        my $file_path=$file_desc->{result}->{file_path};
-        `curl -s -k -o ${path}/${update_id}.jpg https://api.telegram.org/file/bot$token/$file_path`;
-        $res="Image was saved as ${path}/${update_id}.jpg";
-      };
-      $res="Error: $@" if $@;
-    }
-    if (my $text = $u->{message}{text}) { # Text message
-      next if $text !~ m!^/!; # Not a command
-      my ($cmd, @params) = split / /, $text;
-      $res = $commands->{substr ($cmd, 1)} || $commands->{_unknown};
-      # Pass to the subroutine the message object, and the parameters passed to the cmd.
-      try {
-        $res=$res->($u->{message}, @params) if ref $res eq "CODE";
-      } catch {
-        $res="Failed: $_";
-      };
+      if (my $text = $u->{message}{text}) { # Text message
+        next if $text !~ m!^/!; # Not a command
+        my ($cmd, @params) = split / /, $text;
+        $res = $commands->{substr ($cmd, 1)} || $commands->{_unknown};
+        # Pass to the subroutine the message object, and the parameters passed to the cmd.
+        try {
+          $res=$res->($u->{message}, @params) if ref $res eq "CODE";
+        } catch {
+          $res="Failed: $_";
+        };
+      }
     }
     next unless $res;
     my $method = ref $res && $res->{method} ? delete $res->{method} : "sendMessage";
