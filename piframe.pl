f5e2594410478ff25606f5fd33f9fbe7fab950be
glgraca <30938064+glgraca@users.noreply.github.com>
2017-10-09 16:07:50 -0300

    Update piframe.pl
    Added support for captions.

--- piframe.pl
+++ piframe.pl
@@ -7,15 +7,18 @@ use Imager;
 use Imager::ExifOrientation;
 use File::Find;
 use File::Random qw/:all/;
-use MIME::Base64;
 use POSIX 'strftime';
 use Data::Dumper;
+use File::Basename;
+use File::Slurp;
 use strict;
+use utf8;
+use open ':std', ':encoding(utf8)';
 
 my @file_list;
 my $tmp='/home/pi/tmp/tmp.jpg';
 my $root=$ARGV[0].'/';
-my $telegram_root=$ARGV[1].'/';
+my $telegram_root='/data01/tmp/';
 
 my $main = MainWindow->new (
   -title => 'PiFrame',
@@ -49,9 +52,11 @@ my $small_canvas=$main->Canvas(
 $small_canvas->pack(-side=>'bottom');
 
 my $time;
+my $caption;
 
 my $clock=$main->Label(
   -textvariable=>\$time,
+  -wraplength=>$thumb_width,
   -width=>100,
   -height=>100,
   -background=>'black',
@@ -68,31 +73,37 @@ $small_canvas->createImage(0,0,-image=>$thumb,-anchor=>'nw');
 sub scale {
   my ($width, $height, $image)=@_;
   my $scaled=$image->scale(xpixels=>$width,ypixels=>$height,type=>'min');
-  $scaled->write(file=>$tmp, type=>'jpeg');  
+  $scaled->write(file=>$tmp, type=>'jpeg');
 }
 
 sub next_photo {
-  my $path=$root;
+  $caption='';
+  my $filename;
   if(int(rand(2))==1) {
-    $path=$telegram_root;
-  } 
-  my $filename=$path.random_file(-dir=>$path, -check=>qr/\.jpg$/i, -recursive=>1);
-  print "$filename\n"; 
+    $filename=$telegram_root.random_file(-dir=>$telegram_root, -check=>qr/\.jpg$/i, -recursive=>1);
+    my ($name, $path, $suffix) = fileparse($filename, '\.[^\.]*');
+    my $caption_file="${path}${name}.txt";
+    if(-e $caption_file) {
+      $caption=read_file($caption_file,binmode=>':utf8');
+    }
+  } else {
+    $filename=$root.random_file(-dir=>$root, -check=>qr/\.jpg$/i, -recursive=>1);
+  }
   my $image = Imager::ExifOrientation->rotate(path => $filename);
   scale($photo_width, $photo_height, $image);
   $photo->configure(-file=>$tmp);
-  
+
   $filename=$root.random_file(-dir=>$root, -check=>qr/\.jpg$/i, -recursive=>1);
-  print "$filename\n"; 
+
   $image = Imager::ExifOrientation->rotate(path => $filename);
   scale($thumb_width, $thumb_height, $image);
   $thumb->configure(-file=>$tmp);
-  
+
   $main->update();
 }
 
 $canvas->repeat(120000, sub {eval {next_photo()}});
-$canvas->repeat(3000, sub {$time=strftime("%H:%M\n%A\n%d/%m", localtime)});
+$canvas->repeat(3000, sub {$time=strftime("%H:%M\n%A\n%d/%m", localtime)."\n\n$caption"});
 
 next_photo();
 
