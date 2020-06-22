1a98feea56d263bd9c7c61ccd75d66100860f590
glgraca <30938064+glgraca@users.noreply.github.com>
2020-06-22 17:28:09 -0300

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-router=`ip route | awk '/default/ {print $3}'`
-/bin/ping -q -c1 $router > /dev/null
-
-if [ $? -eq  0 ]
-then
-  # echo "Network active"
-  :
+if ifconfig wlan0 | grep -Pq "inet\s\d+\.\d+\.\d+\.\d+" ; then
+  echo "Wifi connected"
 else
-  # echo "Network down, fixing..."
-  /bin/kill -9 `pidof wpa_supplicant`
-  /sbin/ifup --force wlan0
-  /sbin/ip route add default via $router dev wlan0
-  /bin/mount -a
+  ifup --force wlan0
 fi
